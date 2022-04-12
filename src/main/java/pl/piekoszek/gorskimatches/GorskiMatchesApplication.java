927f3b38d8e368bbcfeec57d9903994bc07f79d1
Morshall2508 <maciej.gorski.93@gmail.com>
2022-04-12 18:30:45 +0200

    GM-17: New function to create image and create single 20x700px line has been added.

--- src/main/java/pl/piekoszek/gorskimatches/GorskiMatchesApplication.java
+++ src/main/java/pl/piekoszek/gorskimatches/GorskiMatchesApplication.java
@@ -1,20 +1,13 @@
 package pl.piekoszek.gorskimatches;
 
-import pl.piekoszek.gorskimatches.image.LineImageCreator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
-
-//@SpringBootApplication
+@SpringBootApplication
 public class GorskiMatchesApplication {
 
-	public static void main(String[] args) throws IOException {
-		var createImage = new LineImageCreator();
-		createImage.create();
-//		SpringApplication.run(GorskiMatchesApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(GorskiMatchesApplication.class, args);
 	}
 
 }
-
-
-
