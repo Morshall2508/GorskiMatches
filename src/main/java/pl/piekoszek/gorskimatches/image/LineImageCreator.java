c8c999253d6c41297c8517079e21844b18a16802
Morshall2508 <maciej.gorski.93@gmail.com>
2022-04-14 21:45:01 +0200

    GM-18: Added a change from Buffered image to bytes, the function to show in web browser does not work yet.

--- src/main/java/pl/piekoszek/gorskimatches/image/LineImageCreator.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/LineImageCreator.java
@@ -3,16 +3,18 @@ package pl.piekoszek.gorskimatches.image;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class LineImageCreator {
-    public void create() throws IOException {
+    public byte[] create() throws IOException {
         BufferedImage lineImage = new BufferedImage(200, 800,BufferedImage.TYPE_INT_ARGB);
         Graphics2D drawer = lineImage.createGraphics();
         drawer.setBackground(Color.black);
         drawer.clearRect(90, 50, 20, 700);
-        File file = new File("build" + File.separator + "line.png");
-        ImageIO.write(lineImage,"PNG",file);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(lineImage, "jpg", baos);
+        byte[] bytes = baos.toByteArray();
+        return bytes;
     }
 }
