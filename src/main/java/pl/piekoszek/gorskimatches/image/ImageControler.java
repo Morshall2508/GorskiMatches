7d3e070798653d1d14bef0b7c4c04127871bbf9f
Morshall2508 <maciej.gorski.93@gmail.com>
2022-04-20 21:22:54 +0200

    GM-18: Added a change from Buffered image to bytes, now image shows on the front page when accessed by: http://localhost:8080/api/image.

--- src/main/java/pl/piekoszek/gorskimatches/image/ImageControler.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/ImageControler.java
@@ -1,24 +1,26 @@
 package pl.piekoszek.gorskimatches.image;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-public class ImageControler {
-    class imagecontroler {
-        @RestController
-        @RequestMapping("api")
-        class ImageController {
+@RestController
+@RequestMapping("api")
+class ImageController {
 
-            @GetMapping("image")
-            byte[] getImage() throws IOException {
+    @RequestMapping(value = "image",method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
 
-                var bytes = createImage.create();
-                return bytes;
-            }
+    byte[] getImage() throws IOException {
+        var createImage = new LineImageCreator();
+        var bytes = createImage.create();
 
-        }
+        return bytes;
+
     }
+
 }
+
