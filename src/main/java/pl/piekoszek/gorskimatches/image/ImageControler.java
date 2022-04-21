7025117b42f45bdfef3df3da09008778b40c2269
Morshall2508 <maciej.gorski.93@gmail.com>
2022-04-21 12:04:58 +0200

    GM-18: Added requested changes

--- src/main/java/pl/piekoszek/gorskimatches/image/ImageControler.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/ImageControler.java
@@ -5,22 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.io.IOException;
 
 @RestController
 @RequestMapping("api")
 class ImageController {
-
-    @RequestMapping(value = "image",method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
-
+    @RequestMapping(value = "image", produces = MediaType.IMAGE_PNG_VALUE)
     byte[] getImage() throws IOException {
         var createImage = new LineImageCreator();
         var bytes = createImage.create();
-
         return bytes;
-
     }
-
 }
 
