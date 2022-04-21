d90d016211dd181356580de16a1d34b28d5de872
Morshall2508 <32619008+Morshall2508@users.noreply.github.com>
2022-04-21 14:11:55 +0200

    GM-20: Made LineImageCreator a Spring @Component and injeced it into … (#5)
    * GM-20: Made LineImageCreator a Spring @Component and injeced it into ImageController via constructor.
    
    * GM-20: Made changes to GetMapping

--- src/main/java/pl/piekoszek/gorskimatches/image/ImageController.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/ImageController.java
@@ -1,6 +1,7 @@
 package pl.piekoszek.gorskimatches.image;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
@@ -8,10 +9,15 @@ import java.io.IOException;
 @RestController
 @RequestMapping("api")
 class ImageController {
-    @RequestMapping(value = "image", produces = MediaType.IMAGE_PNG_VALUE)
+
+    private final LineImageCreator lineImageCreator;
+
+    ImageController(LineImageCreator lineImageCreator){
+        this.lineImageCreator = lineImageCreator;
+    }
+    @GetMapping(value = "image", produces = MediaType.IMAGE_PNG_VALUE)
     byte[] getImage() throws IOException {
-        var createImage = new LineImageCreator();
-        return createImage.create();
+        return lineImageCreator.create();
     }
 }
 
--- src/main/java/pl/piekoszek/gorskimatches/image/LineImageCreator.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/LineImageCreator.java
@@ -1,11 +1,13 @@
 package pl.piekoszek.gorskimatches.image;
 
+import org.springframework.stereotype.Component;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
+@Component
 public class LineImageCreator {
     public byte[] create() throws IOException {
         BufferedImage lineImage = new BufferedImage(200, 800,BufferedImage.TYPE_INT_ARGB);
