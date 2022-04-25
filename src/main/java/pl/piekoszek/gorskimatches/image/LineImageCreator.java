d3fd7154d2d6c3b5554f09caae4fa6c160964371
Morshall2508 <maciej.gorski.93@gmail.com>
2022-04-25 18:33:46 +0200

    GM-21: Made so that the drawer draws a number 8

--- src/main/java/pl/piekoszek/gorskimatches/image/LineImageCreator.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/LineImageCreator.java
@@ -12,7 +12,7 @@ public class LineImageCreator {
 
     int[][] newArray = new int[10][];
 
-    public void newArray() {
+    {
         newArray[0] = new int[]{0,1,2,4,5,6};
         newArray[1] = new int[]{1,4};
         newArray[2] = new int[]{0,2,3,4,5};
@@ -21,28 +21,25 @@ public class LineImageCreator {
         newArray[5] = new int[]{0,1,3,5,6};
         newArray[6] = new int[]{0,1,3,4,5,6};
         newArray[7] = new int[]{0,2,6};
-        newArray[8] = new int[]{0,1,2,3,4,5,6,};
-        newArray[9] = new int[]{1,2,3,5,6};
-        }
+        newArray[8] = new int[]{0,1,2,3,4,5,6};
+        newArray[9] = new int[]{0,1,2,3,5,6};
+
+    }
 
     public byte[] create() throws IOException {
-        BufferedImage lineImage = new BufferedImage(200, 800,BufferedImage.TYPE_INT_ARGB);
-
-
-//
-//        Graphics2D drawer = lineImage.createGraphics();
-//        drawer.setBackground(Color.black);
-//        drawer.clearRect(90, 50, 20, 700);
+        BufferedImage lineImage = new BufferedImage(400, 600,BufferedImage.TYPE_INT_ARGB);
+        Graphics2D drawer = lineImage.createGraphics();
+        drawer.setBackground(Color.black);
+        drawer.clearRect(20, 20, 360, 20); /*1st line - 0 */
+        drawer.clearRect(20, 20, 20, 300); /*2nd line */
+        drawer.clearRect(20, 300, 360, 20); /*3rd line */
+        drawer.clearRect(380, 20, 20, 300); /*4th line */
+        drawer.clearRect(20, 300, 20, 300); /*5th line */
+        drawer.clearRect(20, 580, 360, 20); /*6th line */
+        drawer.clearRect(380, 300, 20, 330); /*7th line */
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(lineImage, "png", baos);
         return baos.toByteArray();
     }
-//
-    private Graphics2D singleLineDrawer(int x, int y, int width, int height) {
-        Graphics2D drawer = singleLineDrawer(x,y,width,height);
-        drawer.setBackground(Color.black);
-        drawer.clearRect (x,y,width,height);
-        return drawer;
-    }
-
 }
+
