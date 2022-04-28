17665dc33625d6a6c307801d2fbb0f407e05c47f
Morshall2508 <32619008+Morshall2508@users.noreply.github.com>
2022-04-28 10:21:24 +0200

    GM-21: New feature added that after typing a number it shows up on the screen (#7)
    * GM-21: Made an array, but not sure if that is correct, also I've made a method but I am also unsure about it.
    
    * GM-21: Made so that the drawer draws a number 8 - the array works 
    
    * GM-21: Made so that the drawer draws a number 8
    
    * GM-21: Endpoint returns image /api/number/{number}
    
    * GM-21: Endpoint returns image /api/number/{number} - small changes in index.html added

--- src/main/java/pl/piekoszek/gorskimatches/image/ImageController.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/ImageController.java
@@ -2,23 +2,27 @@ package pl.piekoszek.gorskimatches.image;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/number")
 class ImageController {
 
     private final LineImageCreator lineImageCreator;
 
+
     ImageController(LineImageCreator lineImageCreator){
         this.lineImageCreator = lineImageCreator;
     }
-    @GetMapping(value = "image", produces = MediaType.IMAGE_PNG_VALUE)
-        byte[] getImage() throws IOException {
+    @GetMapping(value = "{number}", produces = MediaType.IMAGE_PNG_VALUE)
+        byte[] getImage(@PathVariable int number) throws IOException {
 
-        return lineImageCreator.create();
+        return lineImageCreator.create(number);
     }
 }
 
--- src/main/java/pl/piekoszek/gorskimatches/image/LineImageCreator.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/LineImageCreator.java
@@ -7,15 +7,61 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
+import static java.awt.Color.black;
+
 @Component
 public class LineImageCreator {
-    public byte[] create() throws IOException {
-        BufferedImage lineImage = new BufferedImage(200, 800,BufferedImage.TYPE_INT_ARGB);
-        Graphics2D drawer = lineImage.createGraphics();
-        drawer.setBackground(Color.black);
-        drawer.clearRect(90, 50, 20, 700);
+
+    LineInfo[][] newArray = new LineInfo[10][];
+
+    {
+        var line0 = new LineInfo(20, 20, true);
+        var line1 = new LineInfo(20, 20, false);
+        var line2 = new LineInfo(20, 280, true);
+        var line3 = new LineInfo(360, 20, false);
+        var line4 = new LineInfo(20, 280, false);
+        var line5 = new LineInfo(20, 560, true);
+        var line6 = new LineInfo(360, 280, false);
+
+        newArray[0] = new LineInfo[]{line0, line1, line3, line4, line5, line6};
+        newArray[1] = new LineInfo[]{line1, line4};
+        newArray[2] = new LineInfo[]{line0, line3, line2, line4, line5};
+        newArray[3] = new LineInfo[]{line0, line3, line2, line6, line5};
+        newArray[4] = new LineInfo[]{line1, line2, line3, line6};
+        newArray[5] = new LineInfo[]{line0, line1, line2, line6, line5};
+        newArray[6] = new LineInfo[]{line0, line1, line4, line2, line5, line6};
+        newArray[7] = new LineInfo[]{line0, line3, line6};
+        newArray[8] = new LineInfo[]{line0, line1, line4, line2, line3, line5, line6};
+        newArray[9] = new LineInfo[]{line0, line1, line2, line3, line5, line6};
+    }
+
+    public byte[] create(int number) throws IOException {
+        BufferedImage lineImage = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
+
+        for (LineInfo lineInfo : newArray[number]) {
+            if (lineInfo.horizontal) {
+                drawHorizontalLine(lineInfo.x, lineInfo.y, lineImage);
+            } else {
+                drawVerticalLine(lineInfo.x, lineInfo.y, lineImage);
+            }
+        }
+
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(lineImage, "png", baos);
         return baos.toByteArray();
     }
+
+    private void drawHorizontalLine(int x, int y, BufferedImage lineImage) {
+        Graphics2D drawLine = lineImage.createGraphics();
+        drawLine.setBackground(black);
+        drawLine.clearRect(x, y, 360, 20);
+    }
+
+    private void drawVerticalLine(int x, int y, BufferedImage lineImage) {
+        Graphics2D drawLine = lineImage.createGraphics();
+        drawLine.setBackground(black);
+        drawLine.clearRect(x, y, 20, 280);
+    }
+
 }
