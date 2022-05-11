177b36f1a906374626d877117c75c2df38a5d82b
Morshall2508 <maciej.gorski.93@gmail.com>
2022-05-11 13:34:43 +0200

    GM-7: Deleted unnecessary lines.

--- src/main/java/pl/piekoszek/gorskimatches/image/EquationRandomizer.java
+++ src/main/java/pl/piekoszek/gorskimatches/image/EquationRandomizer.java
@@ -6,15 +6,11 @@ import java.util.Random;
 
 @Component
 public class EquationRandomizer {
-    public static String main(String[] args) {
-        String[] randomEquations = {"9+2=6", "5+7=2", "9+3=5", "4+6=4", "5+2=8", "1+2=8", "3+2=4", "7-2=4"};
+    public static void main(String[] args) {
+
         for (int i = 0; i <= 0; i++) {
             System.out.println(randomNumber());
-
         }
-        int positionInString = new Random().nextInt(randomEquations.length);
-        String randomNumber = (randomEquations[positionInString]);
-        return randomNumber;
 
     }
 
