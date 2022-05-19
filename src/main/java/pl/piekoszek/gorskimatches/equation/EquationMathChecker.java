e57a66f17827c6669ce992d75d35802436af237e
Morshall2508 <maciej.gorski.93@gmail.com>
2022-05-19 14:55:08 +0200

    GM-26: Implemented EquationMathChecker.

--- src/main/java/pl/piekoszek/gorskimatches/equation/EquationMathChecker.java
+++ src/main/java/pl/piekoszek/gorskimatches/equation/EquationMathChecker.java
@@ -6,10 +6,29 @@ import org.springframework.stereotype.Component;
 // todo maybe find better name
 class EquationMathChecker {
 
+
     boolean isMathematicallyCorrect(String equation) {
 
-        //todo implement
+        char[] equationChar = equation.toCharArray();
+
+        int a = Integer.parseInt(String.valueOf(equationChar[0]));
+        int b = Integer.parseInt(String.valueOf(equationChar[2]));
+        int c = Integer.parseInt(String.valueOf(equationChar[4]));
+
+        if (a + b == c)
+            return true;
+
+        if (a - b == c)
+            return true;
+
+        if (a + b != c)
+            return false;
+
+        if (a - b != c)
+            return false;
         throw new RuntimeException("Not implemented");
     }
 
 }
+
+
