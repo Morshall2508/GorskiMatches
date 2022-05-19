108fa52d4f4a9e97fb53ff71336dbe4d05013a9d
Morshall2508 <maciej.gorski.93@gmail.com>
2022-05-19 17:15:01 +0200

    GM-26: Bug-fix: before it wasn't checking for a "+" or a "-" signs. Now it does and works correct on "2+2=0"

--- src/main/java/pl/piekoszek/gorskimatches/equation/EquationMathChecker.java
+++ src/main/java/pl/piekoszek/gorskimatches/equation/EquationMathChecker.java
@@ -3,7 +3,6 @@ package pl.piekoszek.gorskimatches.equation;
 import org.springframework.stereotype.Component;
 
 @Component
-// todo maybe find better name
 class EquationMathChecker {
 
 
@@ -14,18 +13,19 @@ class EquationMathChecker {
         int a = Integer.parseInt(String.valueOf(equationChar[0]));
         int b = Integer.parseInt(String.valueOf(equationChar[2]));
         int c = Integer.parseInt(String.valueOf(equationChar[4]));
+        if (equation.contains("+")) {
+            if (a + b == c)
+                return true;
+            if (a + b != c)
+                return false;
 
-        if (a + b == c)
-            return true;
+        } else if (equation.contains("-")) {
+            if (a - b == c)
+                return true;
 
-        if (a - b == c)
-            return true;
-
-        if (a + b != c)
-            return false;
-
-        if (a - b != c)
-            return false;
+            if (a - b != c)
+                return false;
+        }
         throw new RuntimeException("Not implemented");
     }
 
