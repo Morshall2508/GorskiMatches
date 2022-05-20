d996ff566a15df147a8d2962302403d35469d6a1
Morshall2508 <32619008+Morshall2508@users.noreply.github.com>
2022-05-20 11:17:22 +0200

    Feature/gm 26 equation math checker (#12)
    * Added EquationMathCheckerTest
    
    * GM-26: Implemented EquationMathChecker.
    
    * GM-26: Bug-fix: before it wasn't checking for a "+" or a "-" signs. Now it does and works correct on "2+2=0"
    
    * GM-26: Implemented changes from review
    
    * GM-26: Implemented changes from review
    
    Co-authored-by: piotriwaniuk <piekoszek@gmail.com>

---
+++ src/main/java/pl/piekoszek/gorskimatches/equation/EquationMathChecker.java
@@ -0,0 +1,27 @@
+package pl.piekoszek.gorskimatches.equation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class EquationMathChecker {
+
+
+    boolean isMathematicallyCorrect(String equation) {
+
+        char[] equationChar = equation.toCharArray();
+
+        int a = Integer.parseInt(String.valueOf(equationChar[0]));
+        int b = Integer.parseInt(String.valueOf(equationChar[2]));
+        int c = Integer.parseInt(String.valueOf(equationChar[4]));
+
+        if (equation.contains("+")) {
+            return a + b == c;
+        } else if (equation.contains("-")) {
+            return a - b == c;
+        }
+        return false;
+    }
+
+}
+
+
