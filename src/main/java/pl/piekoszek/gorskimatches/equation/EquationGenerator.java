80b0c55e230b984c2d848b9e79de324f73a3a6fa
Morshall2508 <maciej.gorski.93@gmail.com>
2022-05-24 12:07:46 +0200

    GM-27: Adjusted as per requests - for loop moved outside of the first loops. Equations and all equations have been removed from class fields and listofMathCorrectEquations has been moved into it. Changed EquationMathChecker to not static.

--- src/main/java/pl/piekoszek/gorskimatches/equation/EquationGenerator.java
+++ src/main/java/pl/piekoszek/gorskimatches/equation/EquationGenerator.java
@@ -1,8 +1,30 @@
 package pl.piekoszek.gorskimatches.equation;
 
-import java.util.ArrayList;
+import org.springframework.stereotype.Component;
+import java.util.*;
 
+@Component
 public class EquationGenerator {
 
+    List<String> listOfMathCorrectEquations = new ArrayList<>();
 
+    EquationGenerator(EquationMathChecker equationMathChecker) {
+        Map<String, Boolean> allEquations = new HashMap<>();
+        List<String> equations = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    equations.add(i + "+" + j + "=" + k);
+                    equations.add(i + "-" + j + "=" + k);
+                }
+            }
+        }
+        for (String equation : equations) {
+            allEquations.put(equation, equationMathChecker.isMathematicallyCorrect(equation));
+            allEquations.remove(equation, false);
+        }
+        Set<String> keySet = allEquations.keySet();
+        List<String> listOfMathCorrectEquations = new ArrayList<>(keySet);
+    }
 }
--- src/main/java/pl/piekoszek/gorskimatches/equation/EquationMathChecker.java
+++ src/main/java/pl/piekoszek/gorskimatches/equation/EquationMathChecker.java
@@ -2,36 +2,11 @@ package pl.piekoszek.gorskimatches.equation;
 
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @Component
 class EquationMathChecker {
 
-    Map<String, Boolean> allEquations = new HashMap<>();
-    ArrayList equations = new ArrayList();
-
-    {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                for (int k = 0; k < 10; k++) {
-                    equations.add(i + "+" + j + "=" + k);
-                    equations.add(i + "-" + j + "=" + k);
-                    for (int l = 0; l < equations.size(); l++) {
-                        allEquations.put((String) equations.get(l), isMathematicallyCorrect((String) equations.get(l)));
-                        allEquations.remove(equations.get(l), false);
-
-                    }
-                }
-            }
-        }
-        Set<String> keySet = allEquations.keySet();
-        ArrayList<String> listOfMathCorrEquations = new ArrayList<String>(keySet);
-    }
-
-    static boolean isMathematicallyCorrect(String equation) {
+    boolean isMathematicallyCorrect(String equation) {
 
         char[] equationChar = equation.toCharArray();
 
