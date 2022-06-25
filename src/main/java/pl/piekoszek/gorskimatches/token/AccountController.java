8350e2c36901f69eaf8ae90d7b3ff47bc5ca12c7
Morshall2508 <32619008+Morshall2508@users.noreply.github.com>
2022-06-25 21:27:12 +0200

    Feature/gm 32 better front end design, make it look good on mobile (#19)
    * GM-32: Pushing current state of code. Now is to make mobile version as well as adjust the scripts
    
    * GM-32: Pushing current state of code. The mobile version is done - it's not supper pretty but will suffice I think.
    
    * GM-32: Now user can see his account name in account page(will do this for all pages - inside navigationBar).
    
    * GM-32: Now user can see page in a new look.
    
    * GM-32: Made small changes.

--- src/main/java/pl/piekoszek/gorskimatches/token/AccountController.java
+++ src/main/java/pl/piekoszek/gorskimatches/token/AccountController.java
@@ -3,6 +3,8 @@ package pl.piekoszek.gorskimatches.token;
 import org.springframework.web.bind.annotation.*;
 import pl.piekoszek.gorskimatches.repository.AccountRepository;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("api/auth")
 class AccountController {
@@ -16,12 +18,17 @@ class AccountController {
     }
 
     @PostMapping("email")
-    void sendRegistrationEmail (@RequestBody AccountInfo accountInfo) {
+    void sendRegistrationEmail(@RequestBody AccountInfo accountInfo) {
         emailService.sendRegistrationOrLoginLink(accountInfo.getEmail());
     }
 
     @PostMapping("account")
     void changeAccountInfo(@RequestBody AccountInfo accountInfo) {
         accountRepository.save(accountInfo);
+    }
+
+    @GetMapping("accountInfo/{email}")
+    AccountInfo fetchAccountInfo(@PathVariable("email") AccountInfo accountInfo) {
+        return accountRepository.findById(accountInfo.getEmail()).get();
     }
 }
