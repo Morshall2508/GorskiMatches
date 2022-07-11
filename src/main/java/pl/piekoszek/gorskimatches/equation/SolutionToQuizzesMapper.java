bc948f4aeeed71321a65cd71ae3887c31bf0d99b
Morshall2508 <maciej.gorski.93@gmail.com>
2022-07-11 11:09:13 +0200

    GM-25: Applied requested changes.

--- src/main/java/pl/piekoszek/gorskimatches/equation/SolutionToQuizzesMapper.java
+++ src/main/java/pl/piekoszek/gorskimatches/equation/SolutionToQuizzesMapper.java
@@ -19,50 +19,50 @@ public class SolutionToQuizzesMapper {
 
     {
         numberOrSymbolChangeableToOther.put('0', List.of('6', '9'));
-        numberOrSymbolChangeableToOther.put('1', new ArrayList<>());
+        numberOrSymbolChangeableToOther.put('1', Collections.emptyList());
         numberOrSymbolChangeableToOther.put('2', List.of('2'));
         numberOrSymbolChangeableToOther.put('3', List.of('2', '5'));
-        numberOrSymbolChangeableToOther.put('4', new ArrayList<>());
+        numberOrSymbolChangeableToOther.put('4', Collections.emptyList());
         numberOrSymbolChangeableToOther.put('5', List.of('3'));
         numberOrSymbolChangeableToOther.put('6', List.of('0', '9'));
-        numberOrSymbolChangeableToOther.put('7', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('8', new ArrayList<>());
+        numberOrSymbolChangeableToOther.put('7', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('8', Collections.emptyList());
         numberOrSymbolChangeableToOther.put('9', List.of('0', '6'));
-        numberOrSymbolChangeableToOther.put('+', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('-', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('=', new ArrayList<>());
+        numberOrSymbolChangeableToOther.put('+', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('-', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('=', Collections.emptyList());
     }
 
     {
-        takeOneMatchFromNumberOrSymbol.put('0', new ArrayList<>());
-        takeOneMatchFromNumberOrSymbol.put('1', new ArrayList<>());
-        takeOneMatchFromNumberOrSymbol.put('2', new ArrayList<>());
-        takeOneMatchFromNumberOrSymbol.put('3', new ArrayList<>());
-        takeOneMatchFromNumberOrSymbol.put('4', new ArrayList<>());
-        takeOneMatchFromNumberOrSymbol.put('5', new ArrayList<>());
+        takeOneMatchFromNumberOrSymbol.put('0', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('1', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('2', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('3', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('4', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('5', Collections.emptyList());
         takeOneMatchFromNumberOrSymbol.put('6', List.of('5'));
         takeOneMatchFromNumberOrSymbol.put('7', List.of('1'));
         takeOneMatchFromNumberOrSymbol.put('8', List.of('0', '6', '9'));
         takeOneMatchFromNumberOrSymbol.put('9', List.of('5'));
         takeOneMatchFromNumberOrSymbol.put('+', List.of('-'));
-        takeOneMatchFromNumberOrSymbol.put('-', new ArrayList<>());
-        takeOneMatchFromNumberOrSymbol.put('=', new ArrayList<>());
+        takeOneMatchFromNumberOrSymbol.put('-', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('=', Collections.emptyList());
     }
 
     {
         giveOneMatchFromNumberOrSymbol.put('0', List.of('8'));
         giveOneMatchFromNumberOrSymbol.put('1', List.of('7'));
-        giveOneMatchFromNumberOrSymbol.put('2', new ArrayList<>());
+        giveOneMatchFromNumberOrSymbol.put('2', Collections.emptyList());
         giveOneMatchFromNumberOrSymbol.put('3', List.of('9'));
-        giveOneMatchFromNumberOrSymbol.put('4', new ArrayList<>());
+        giveOneMatchFromNumberOrSymbol.put('4', Collections.emptyList());
         giveOneMatchFromNumberOrSymbol.put('5', List.of('6', '9'));
         giveOneMatchFromNumberOrSymbol.put('6', List.of('8'));
-        giveOneMatchFromNumberOrSymbol.put('7', new ArrayList<>());
-        giveOneMatchFromNumberOrSymbol.put('8', new ArrayList<>());
+        giveOneMatchFromNumberOrSymbol.put('7', Collections.emptyList());
+        giveOneMatchFromNumberOrSymbol.put('8', Collections.emptyList());
         giveOneMatchFromNumberOrSymbol.put('9', List.of('8'));
-        giveOneMatchFromNumberOrSymbol.put('+', new ArrayList<>());
+        giveOneMatchFromNumberOrSymbol.put('+', Collections.emptyList());
         giveOneMatchFromNumberOrSymbol.put('-', List.of('+'));
-        giveOneMatchFromNumberOrSymbol.put('=', new ArrayList<>());
+        giveOneMatchFromNumberOrSymbol.put('=', Collections.emptyList());
     }
 
     public Map<String, Set<String>> insideSingleNumber(String solution) {
@@ -87,19 +87,18 @@ public class SolutionToQuizzesMapper {
             char numberOrSymbolToBeReplaced = solution.charAt(i);
             var numbersAndSymbolsWithTakenMatch = takeOneMatchFromNumberOrSymbol.get(numberOrSymbolToBeReplaced);
 
-            for (Character numberWithTakenMatch : numbersAndSymbolsWithTakenMatch) {
+            for (char numberWithTakenMatch : numbersAndSymbolsWithTakenMatch) {
                 String quizWithTakenMatch = characterChanger.changeCharactersInString(solution, i, numberWithTakenMatch);
                 for (int k = 0; k < quizWithTakenMatch.length(); k++) {
                     if (k != i) {
                         char numberOrSymbolToAddMatchTo = quizWithTakenMatch.charAt(k);
                         var numbersAndSymbolsWithAddedMatch = giveOneMatchFromNumberOrSymbol.get(numberOrSymbolToAddMatchTo);
-                        for (Character matchReceiver : numbersAndSymbolsWithAddedMatch) {
-                            String quizWithGivenMatch = characterChanger.changeCharactersInString(quizWithTakenMatch, k, matchReceiver);
-                            if (equationMathChecker.isMathematicallyCorrect(quizWithGivenMatch)) {
-                                continue;
+                        for (char characterOrSymbolWithAddedMatch : numbersAndSymbolsWithAddedMatch) {
+                            String quizWithGivenMatch = characterChanger.changeCharactersInString(quizWithTakenMatch, k, characterOrSymbolWithAddedMatch);
+                            if (!equationMathChecker.isMathematicallyCorrect(quizWithGivenMatch)) {
+                                quizzesAndSolutionsWithinEquation.putIfAbsent(quizWithGivenMatch, new HashSet<>());
+                                quizzesAndSolutionsWithinEquation.get(quizWithGivenMatch).add(solution);
                             }
-                            quizzesAndSolutionsWithinEquation.putIfAbsent(quizWithGivenMatch, new HashSet<>());
-                            quizzesAndSolutionsWithinEquation.get(quizWithGivenMatch).add(solution);
                         }
                     }
                 }
