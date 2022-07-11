bbcf832f52d9a925d8ae988ac85a257370b9efce
Morshall2508 <32619008+Morshall2508@users.noreply.github.com>
2022-07-11 14:40:28 +0200

    Feature/gm 25 generate quizes (change between different matches) (#21)
    * GM-25: WIP. Added new Maps, and began to create method.
    
    * GM-25: WIP.
    
    * GM-25: Now it generates quizzes that can be solved by moving a match within equation.
    
    * GM-25: Applied requested changes.
    
    * GM-25: Applied requested changes.

--- src/main/java/pl/piekoszek/gorskimatches/equation/SolutionToQuizzesMapper.java
+++ src/main/java/pl/piekoszek/gorskimatches/equation/SolutionToQuizzesMapper.java
@@ -7,35 +7,62 @@ import java.util.*;
 @Component
 public class SolutionToQuizzesMapper {
     private final CharacterChanger characterChanger;
+    private final EquationMathChecker equationMathChecker;
     private final Map<Character, List<Character>> numberOrSymbolChangeableToOther = new HashMap<>();
-    SolutionToQuizzesMapper(CharacterChanger characterChanger) {
+    private final Map<Character, List<Character>> takeOneMatchFromNumberOrSymbol = new HashMap<>();
+    private final Map<Character, List<Character>> giveOneMatchFromNumberOrSymbol = new HashMap<>();
+
+    SolutionToQuizzesMapper(CharacterChanger characterChanger, EquationMathChecker equationMathChecker) {
         this.characterChanger = characterChanger;
+        this.equationMathChecker = equationMathChecker;
     }
 
     {
-        numberOrSymbolChangeableToOther.put('0', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('1', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('2', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('3', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('5', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('6', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('7', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('8', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('9', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('+', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('-', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('=', new ArrayList<>());
-        numberOrSymbolChangeableToOther.put('4', new ArrayList<>());
-        numberOrSymbolChangeableToOther.get('0').add('6');
-        numberOrSymbolChangeableToOther.get('0').add('9');
-        numberOrSymbolChangeableToOther.get('6').add('0');
-        numberOrSymbolChangeableToOther.get('6').add('9');
-        numberOrSymbolChangeableToOther.get('9').add('0');
-        numberOrSymbolChangeableToOther.get('9').add('6');
-        numberOrSymbolChangeableToOther.get('3').add('5');
-        numberOrSymbolChangeableToOther.get('3').add('2');
-        numberOrSymbolChangeableToOther.get('5').add('3');
-        numberOrSymbolChangeableToOther.get('2').add('3');
+        numberOrSymbolChangeableToOther.put('0', List.of('6', '9'));
+        numberOrSymbolChangeableToOther.put('1', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('2', List.of('2'));
+        numberOrSymbolChangeableToOther.put('3', List.of('2', '5'));
+        numberOrSymbolChangeableToOther.put('4', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('5', List.of('3'));
+        numberOrSymbolChangeableToOther.put('6', List.of('0', '9'));
+        numberOrSymbolChangeableToOther.put('7', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('8', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('9', List.of('0', '6'));
+        numberOrSymbolChangeableToOther.put('+', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('-', Collections.emptyList());
+        numberOrSymbolChangeableToOther.put('=', Collections.emptyList());
+    }
+
+    {
+        takeOneMatchFromNumberOrSymbol.put('0', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('1', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('2', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('3', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('4', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('5', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('6', List.of('5'));
+        takeOneMatchFromNumberOrSymbol.put('7', List.of('1'));
+        takeOneMatchFromNumberOrSymbol.put('8', List.of('0', '6', '9'));
+        takeOneMatchFromNumberOrSymbol.put('9', List.of('5'));
+        takeOneMatchFromNumberOrSymbol.put('+', List.of('-'));
+        takeOneMatchFromNumberOrSymbol.put('-', Collections.emptyList());
+        takeOneMatchFromNumberOrSymbol.put('=', Collections.emptyList());
+    }
+
+    {
+        giveOneMatchFromNumberOrSymbol.put('0', List.of('8'));
+        giveOneMatchFromNumberOrSymbol.put('1', List.of('7'));
+        giveOneMatchFromNumberOrSymbol.put('2', Collections.emptyList());
+        giveOneMatchFromNumberOrSymbol.put('3', List.of('9'));
+        giveOneMatchFromNumberOrSymbol.put('4', Collections.emptyList());
+        giveOneMatchFromNumberOrSymbol.put('5', List.of('6', '9'));
+        giveOneMatchFromNumberOrSymbol.put('6', List.of('8'));
+        giveOneMatchFromNumberOrSymbol.put('7', Collections.emptyList());
+        giveOneMatchFromNumberOrSymbol.put('8', Collections.emptyList());
+        giveOneMatchFromNumberOrSymbol.put('9', List.of('8'));
+        giveOneMatchFromNumberOrSymbol.put('+', Collections.emptyList());
+        giveOneMatchFromNumberOrSymbol.put('-', List.of('+'));
+        giveOneMatchFromNumberOrSymbol.put('=', Collections.emptyList());
     }
 
     public Map<String, Set<String>> insideSingleNumber(String solution) {
@@ -51,5 +78,32 @@ public class SolutionToQuizzesMapper {
             }
         }
         return quizzesAndSolutions;
+    }
+
+    public Map<String, Set<String>> insideEquation(String solution) {
+        Map<String, Set<String>> quizzesAndSolutionsWithinEquation = new HashMap<>();
+
+        for (int i = 0; i < solution.length(); i++) {
+            char numberOrSymbolToBeReplaced = solution.charAt(i);
+            var numbersAndSymbolsWithTakenMatch = takeOneMatchFromNumberOrSymbol.get(numberOrSymbolToBeReplaced);
+
+            for (char numberWithTakenMatch : numbersAndSymbolsWithTakenMatch) {
+                String quizWithTakenMatch = characterChanger.changeCharactersInString(solution, i, numberWithTakenMatch);
+                for (int k = 0; k < quizWithTakenMatch.length(); k++) {
+                    if (k != i) {
+                        char numberOrSymbolToAddMatchTo = quizWithTakenMatch.charAt(k);
+                        var numbersAndSymbolsWithAddedMatch = giveOneMatchFromNumberOrSymbol.get(numberOrSymbolToAddMatchTo);
+                        for (char characterOrSymbolWithAddedMatch : numbersAndSymbolsWithAddedMatch) {
+                            String quizWithGivenMatch = characterChanger.changeCharactersInString(quizWithTakenMatch, k, characterOrSymbolWithAddedMatch);
+                            if (!equationMathChecker.isMathematicallyCorrect(quizWithGivenMatch)) {
+                                quizzesAndSolutionsWithinEquation.putIfAbsent(quizWithGivenMatch, new HashSet<>());
+                                quizzesAndSolutionsWithinEquation.get(quizWithGivenMatch).add(solution);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return quizzesAndSolutionsWithinEquation;
     }
 }
--- src/test/java/pl/piekoszek/gorskimatches/equation/SolutionToQuizzesMapperTest.java
+++ src/test/java/pl/piekoszek/gorskimatches/equation/SolutionToQuizzesMapperTest.java
@@ -2,12 +2,13 @@ package pl.piekoszek.gorskimatches.equation;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SolutionToQuizzesMapperTest {
 
-    private final SolutionToQuizzesMapper solutionToQuizzesMapper = new SolutionToQuizzesMapper(new CharacterChanger());
-    
+    private final SolutionToQuizzesMapper solutionToQuizzesMapper = new SolutionToQuizzesMapper(new CharacterChanger(), new EquationMathChecker());
+
     @Test
     void shouldCreateQuizzesFromSolutionByMovingMatchInsideOneCharacter() {
         var quizSolutionMap = solutionToQuizzesMapper.insideSingleNumber("6+1=7");
@@ -33,4 +34,68 @@ public class SolutionToQuizzesMapperTest {
         assertThat(quizSolutionMap).hasSize(3);
     }
 
+    @Test
+    void shouldCreateQuizzesFromSolutionByMovingMatchInsideWholeEquation() {
+        var quizSolutionMap = solutionToQuizzesMapper.insideEquation("5+3=8");
+        assertThat(quizSolutionMap).hasSize(11);
+        assertThat(quizSolutionMap.get("9-3=8")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("6-3=8")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("5-9=8")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("9+3=0")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("6+3=0")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("5+9=0")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("9+3=9")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("5+9=9")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("9+3=6")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("6+3=6")).containsExactly("5+3=8");
+        assertThat(quizSolutionMap.get("5+9=6")).containsExactly("5+3=8");
+
+    }
+
+    @Test
+    void shouldCreateQuizzesFromSolutionByMovingMatchInsideWholeEquation2() {
+        var quizSolutionMap = solutionToQuizzesMapper.insideEquation("2+3=5");
+        assertThat(quizSolutionMap).hasSize(3);
+        assertThat(quizSolutionMap.get("2-9=5")).containsExactly("2+3=5");
+        assertThat(quizSolutionMap.get("2-3=6")).containsExactly("2+3=5");
+        assertThat(quizSolutionMap.get("2-3=9")).containsExactly("2+3=5");
+    }
+
+    @Test
+    void shouldCreateQuizzesFromSolutionByMovingMatchInsideWholeEquation3() {
+        var quizSolutionMap = solutionToQuizzesMapper.insideEquation("6+3=9");
+        assertThat(quizSolutionMap).hasSize(6);
+        assertThat(quizSolutionMap.get("5+9=9")).containsExactly("6+3=9");
+        assertThat(quizSolutionMap.get("8+3=5")).containsExactly("6+3=9");
+        assertThat(quizSolutionMap.get("6+9=5")).containsExactly("6+3=9");
+        assertThat(quizSolutionMap.get("8-3=9")).containsExactly("6+3=9");
+        assertThat(quizSolutionMap.get("6-9=9")).containsExactly("6+3=9");
+        assertThat(quizSolutionMap.get("6-3=8")).containsExactly("6+3=9");
+
+    }
+
+    @Test
+    void shouldCreateQuizzesFromSolutionByMovingMatchInsideWholeEquation4() {
+        var quizSolutionMap = solutionToQuizzesMapper.insideEquation("7-1=6");
+        assertThat(quizSolutionMap).hasSize(5);
+        assertThat(quizSolutionMap.get("1+1=6")).containsExactly("7-1=6");
+        assertThat(quizSolutionMap.get("1-7=6")).containsExactly("7-1=6");
+        assertThat(quizSolutionMap.get("1-1=8")).containsExactly("7-1=6");
+        assertThat(quizSolutionMap.get("7-7=5")).containsExactly("7-1=6");
+        assertThat(quizSolutionMap.get("7+1=5")).containsExactly("7-1=6");
+
+    }
+
+    @Test
+    void shouldCreateQuizzesFromSolutionByMovingMatchInsideWholeEquation5() {
+        var quizSolutionMap = solutionToQuizzesMapper.insideEquation("0+8=8");
+        assertThat(quizSolutionMap).hasSize(6);
+        assertThat(quizSolutionMap.get("8+6=8")).containsExactly("0+8=8");
+        assertThat(quizSolutionMap.get("8+9=8")).containsExactly("0+8=8");
+        assertThat(quizSolutionMap.get("8-8=8")).containsExactly("0+8=8");
+        assertThat(quizSolutionMap.get("8+8=6")).containsExactly("0+8=8");
+        assertThat(quizSolutionMap.get("8+8=9")).containsExactly("0+8=8");
+        assertThat(quizSolutionMap.get("8+8=9")).containsExactly("0+8=8");
+
+    }
 }
